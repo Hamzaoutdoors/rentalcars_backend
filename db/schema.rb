2e8a28a6bdbeeca36d1d39138b214ed3c237990c
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-31 17:46:11 -0300

    feature: update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_161012) do
+ActiveRecord::Schema.define(version: 2022_01_31_204022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2022_01_25_161012) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "cars", "users"
