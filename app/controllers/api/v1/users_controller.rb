5d72c85b48fe2174c3b5e6a1c97c895206d6b79d
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-31 21:30:08 -0300

    feature: change login action to return cookies and render a response with username and email of user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,9 +20,10 @@ class Api::V1::UsersController < ApplicationController
 
     if user && user.authenticate(params[:user][:password])
       token = encode_token({ user_id: user.id })
-      render json: { user: user, token: token }
+      cookies.signed[:jwt] = { value:  token, httponly: true, expires: 1.hour.from_now }
+      render json: { username: user.username, email: user.email }
     else
-      render json: { error: 'Invalid username or password' }
+      render json: { error: 'Invalid username or password', status: :user_not_found }
     end
   end
 
