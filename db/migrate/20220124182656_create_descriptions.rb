c38a769bc016c0905e1614859eebe757bc31a8d4
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-02-02 12:34:26 -0300

    feature: run migrations

--- db/migrate/20220124182656_create_descriptions.rb
+++ db/migrate/20220124182656_create_descriptions.rb
@@ -1,7 +1,7 @@
 class CreateDescriptions < ActiveRecord::Migration[6.1]
   def change
     create_table :descriptions do |t|
-      t.string :insurance_fee
+      t.decimal  :insurance_fee
       t.decimal :price_daily
       t.decimal :price_monthly
       t.string :color
