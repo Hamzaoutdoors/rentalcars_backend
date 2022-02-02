823a47f1e34a5ee1d47128e17269ddf8335dc765
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-02-02 09:22:52 -0300

    feat: add index on users email:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_204022) do
+ActiveRecord::Schema.define(version: 2022_02_02_122127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2022_01_31_204022) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email"
   end
 
   add_foreign_key "cars", "users"
