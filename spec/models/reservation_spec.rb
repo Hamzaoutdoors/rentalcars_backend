e523ad353a59b6f3575bc0de4c5cc85e26b191f3
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2022-02-04 20:37:12 +0500

    Add specs to user and reservations models

---
+++ spec/models/reservation_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Reservation, type: :model do
+  before :each do
+    user = User.new(id: 1, username: 'hamza')
+    user.email = 'foo1@foo.com'
+    user.password = 'admin123'
+    user.save
+  end
+
+  subject do
+    Reservation.new(
+      start_date: '2022-02-02',
+      end_date: '2022-02-06',
+      user_id: 1, 
+      car_id: 1
+    )
+  end
+
+  before { subject.save }
+
+  it 'is not valid with invalid name' do
+    subject.start_date = nil
+    expect(subject).to_not be_valid
+  end 
+   
+  
+  it 'is not valid with invalid end date' do
+    subject.end_date = nil
+    expect(subject).to_not be_valid
+  end
+end
