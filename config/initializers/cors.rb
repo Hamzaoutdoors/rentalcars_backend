c26f484e8773579567616c4d082287aace723bcd
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-02-04 10:25:43 -0300

    feat: update cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,13 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3000'
     resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
   allow do
-      origins 'https://my-app-frontend.netlify.app'
+      origins 'http://[::1]:4000'
       resource '*',
             headers: :any,
             methods: [:get, :post, :put, :patch, :delete, :options, :head]
