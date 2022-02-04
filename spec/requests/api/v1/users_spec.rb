933d0a529f6d6e1bee68fca1fb2d475ae4474f24
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2022-02-04 20:12:10 +0500

    Add api documetation to users

---
+++ spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,76 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/users', type: :request do
+  let(:user) { create(:user) }
+  let(:user1) { create(:user, username: 'Qwerty') }
+  let(:Authorization) { "Bearer #{JWT.encode({user_id: user[:id]}, 'HaNJLisLook1ng')}" }
+  path '/signup' do
+
+    post('create user') do
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :data, in: :body, schema: {
+        type: :object,
+        properties: {
+          user: {
+            type: :object,
+            properties: {
+              username: { type: :string },
+              email: { type: :string },
+              password: { type: :string}
+            },
+            required: [ 'username', 'email', 'password' ]
+          }
+        },
+        required: ['user']
+      }
+      
+      response(201, 'successful') do
+        let(:data) { {
+          user: { username: 'FakeUser', email: 'fake@email.com',  password: 'password'}
+          }}
+        run_test!
+      end     
+    end
+  end
+
+
+  path '/login' do
+    post('login user') do
+      consumes 'application/json'
+      produces 'application/json'
+      security [ bearer_auth: [] ]
+      parameter name: :data, in: :body, schema: {
+        type: :object,
+        properties: {
+          user: {
+            type: :object,
+            properties: {
+              username: {type: :string},
+              email: { type: :string },
+              password: { type: :string}
+            },
+            required: [ 'email', 'password']
+          }
+        },
+        required: ['user']
+      }
+      
+      response(201, 'successful') do
+        let(:data) { {
+          user: { username: 'Bugos', email: 'Begos@bogos.com',  password: 'Bigos'}
+          }}
+        run_test!
+      end     
+
+      response(401, 'unauthorized') do
+        let(:data) { {
+          user: {username: 'FakeUser', email: 'fake@email.com',  password: 'password'}
+          }}
+        run_test!
+      end
+      
+    end
+  end  
+
+end
